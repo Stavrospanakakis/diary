23ea3fdc61ce299ab64f38ea9e7f6e5973537b41
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-04 19:11:33 +0300

    Add file that exports the components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Layout } from './Layout/Layout'
+export { default as Post } from './Post/Post'
+export { default as PostPreview } from './PostPreview/PostPreview'
+export { default as Navbar } from './Navbar/Navbar'
