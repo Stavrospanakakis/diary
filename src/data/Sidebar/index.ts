7c536ec731ed704a21cfe2ddbf9563aa60a77bc9
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-05 02:12:48 +0300

    Add sidebar data

---
+++ src/data/Sidebar/index.ts
@@ -0,0 +1,32 @@
+const SidebarData = {
+    
+    fullName: 'John Doe',
+    profileImage: 'profile.jpg',
+    property: 'Web Developer',
+    about: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    socialMedia: [
+        {
+            name: 'Linkedin',
+            icon: 'fa fa-linkedin',
+            link: 'https://www.linkedin.com'
+        },
+        {
+            name: 'Instagram',
+            icon: 'fa fa-instagram',
+            link: 'www.instagram.com/'
+        },
+        {
+            name: 'Twitter',
+            icon: 'fa fa-twitter',
+            link: 'https://twitter.com'
+        },
+        {
+            name: 'Github',
+            icon: 'fa fa-github',
+            link: 'https://github.com'
+        }
+    ]
+      
+}
+
+export default SidebarData
