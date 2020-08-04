237b41a083709c9bc15afc5e84d7ac092dde26c6
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-04 19:11:03 +0300

    Add post component

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { graphql} from "gatsby"
+import Layout from '../Layout/Layout'
+
+export const postQuery = graphql`
+  query Post($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path }}) {
+      frontmatter {
+        date
+        title
+        path
+      }
+      html
+    }
+  }
+`
+
+interface IPostProps {
+    data: any
+}
+
+const Post: React.FC<IPostProps> = props => {
+    const { data } = props
+    const post = data.markdownRemark
+    const { title, date } = post.frontmatter
+    return (
+        <Layout>
+            <div className='post'>
+                <h1 className='post__title'>{title}</h1>
+                <p className='post__date'>{date}</p>
+                <div dangerouslySetInnerHTML={{__html: post.html}}></div>
+            </div>
+           
+        </Layout>
+    )
+}
+
+export default Post
