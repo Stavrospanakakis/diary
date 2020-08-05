56c4d302c82c4fbce3bb1a70d151a96822b0bf00
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-05 20:08:00 +0300

    Change sidebar's HTML structure

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import SidebarData from '../../data/Sidebar'
 
 const Sidebar: React.FC = () => {
-    const {profileImage, fullName, property, about, socialMedia} = SidebarData
-    const imgSrc = require('../../images/' + profileImage)
+    const {fullName, profileImage, property, about, socialMedia} = SidebarData
+    
     return (
         <div className='sidebar'>
+            <div className='sidebar__header'>About me</div>
 
-            <img className='sidebar__profileImage' src={imgSrc} alt={fullName}/>
+            <img 
+                className='sidebar__profileImage' 
+                src={profileImage} 
+                alt={fullName}
+            />
+
             <div className='sidebar__fullName'>{fullName}</div>
             <div className='sidebar__property'>{property}</div>
-            <div className='sidebar__about'>About</div>
             <div className='sidebar__info'>{about}</div>
-            <div className='sidebar__find'>Find me on:</div>
-            {socialMedia.map(social => 
-            <div className='sidebar__social-media'>
-               <i className={social.icon}></i><a className='sidebar__social-media__social'href={social.link}>{social.name}</a>    
-            </div>
+
+            <div className='sidebar__header'>Contact me</div>
+            {socialMedia.map((social, index) => 
+                <div key={index} className='sidebar__social-media'>
+
+                <a href={social.link} className='sidebar__social-media__social'>
+                    <i className={social.icon}></i>
+                </a>   
+                </div>
             )}
         </div>
     )
