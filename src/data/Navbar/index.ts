dff2e35006c25dcb117060d5f8b49169f1b9a796
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-05 22:07:59 +0300

    Add navbar data

---
+++ src/data/Navbar/index.ts
@@ -0,0 +1,15 @@
+const NavbarData = {
+    title: 'Diary of a Web Developer',
+    pages: [
+        {
+            name: 'Projects',
+            href: 'https://github.com'
+        },
+        {
+            name: 'Resume',
+            href: 'https://resume.com'
+        },
+    ]
+}
+
+export default NavbarData
