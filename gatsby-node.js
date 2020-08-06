29b11f1d0940463a4561c1f06b224f95c76db00a
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-06 15:55:42 +0300

    Install dependancies/Change settings for eslint

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 // Creates a page for each blog post using the "Post" Component
 exports.createPages = ({ actions, graphql }) => {
-    const { createPage } = actions
+	const { createPage } = actions
   
-    const postTemplate = path.resolve('src/components/Post/Post.tsx')
-    return graphql(`
+	const postTemplate = path.resolve('src/components/Post/Post.tsx')
+	return graphql(`
     {
       allMarkdownRemark {
         edges {
@@ -18,14 +18,14 @@ exports.createPages = ({ actions, graphql }) => {
       }
     }
   `).then(res => {
-    if (res.errors) {
-      return  Promise.reject(res.errors)
-    }
-    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: postTemplate,
-      })
-    })
-  })
+		if (res.errors) {
+			return  Promise.reject(res.errors)
+		}
+		res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+			createPage({
+				path: node.frontmatter.path,
+				component: postTemplate,
+			})
+		})
+	})
 }
