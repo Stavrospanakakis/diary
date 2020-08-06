4f1cbee56fa8e5254da84c3a5e15423e3292c159
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-06 20:59:22 +0300

    Add unit test for post preview

---
+++ src/components/PostPreview/PostPreview.test.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { render } from '@testing-library/react'
+
+import PostPreview from './PostPreview'
+
+describe('PostPreview', () => {
+
+	it('should render', () => {
+		const { container } =
+			render(
+				<PostPreview
+					image='test_image.jpg'
+					title='test title'
+					description='test description'
+					path='test path'
+					tags={['these', 'are', 'test', 'tags']}
+				/>
+			)
+		expect(container).toMatchSnapshot()
+	})
+})
