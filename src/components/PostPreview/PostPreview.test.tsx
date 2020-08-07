419ecc0d9664b46a71612da2c782098c77caeb2a
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-07 19:09:51 +0300

    Change Post preview test

--- src/components/PostPreview/PostPreview.test.tsx
+++ src/components/PostPreview/PostPreview.test.tsx
@@ -4,19 +4,17 @@ import { render } from '@testing-library/react'
 
 import PostPreview from './PostPreview'
 
-describe('PostPreview', () => {
-
+describe('Navbar', () => {
 	it('should render', () => {
-		const { container } =
-			render(
-				<PostPreview
-					image='test_image.jpg'
-					title='test title'
-					description='test description'
-					path='test path'
-					tags={['these', 'are', 'test', 'tags']}
-				/>
-			)
+		const { container } = render(
+			<PostPreview
+				image='test_image.jpg'
+				title='test title'
+				description='test description'
+				path='test path'
+				tags={['these', 'are', 'test', 'tags']}
+			/>)
 		expect(container).toMatchSnapshot()
 	})
 })
+
