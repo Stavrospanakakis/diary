28eccee1e210e7e23160ec06ca3e175767f9717e
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-08 18:26:34 +0300

    Change tests configuration

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,12 @@ module.exports = {
 	transform: { '\\.ts$': ['ts-jest'] },
 	testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+	moduleNameMapper: {
+		'.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+		'.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/file-mock.js',
+	},
+	testPathIgnorePatterns: ['node_modules', '\\.cache', '<rootDir>.*/public'],
+	transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
 	globals: {
 		'ts-jest': {
 			tsConfig: {
