bac8f6056f74e2e907b835da55e5c3f07668d96d
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-08 18:28:17 +0300

    Prototype landpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { graphql} from 'gatsby'
+import { Layout, PostPreview } from '../components/index'
 
-const Landpage: React.FC = () => {
+export const postsPreview = graphql`
+query AllPostsPreview {
+  allMarkdownRemark {
+    edges {
+      node {
+        frontmatter {
+          description
+          image
+          path
+          tags
+          title
+        }
+      }
+    }
+  }
+}
+`
+
+interface ILandpageProps {
+    data: any
+}
+
+const Landpage: React.FC<ILandpageProps> = props => {
+	const { data } = props
 	return (
-		<h1>SOON</h1>
+		<Layout>
+			{ 
+				data.allMarkdownRemark.edges.map(post => 
+					<PostPreview
+						image={post.node.frontmatter.image}
+						title={post.node.frontmatter.title}
+						description={post.node.frontmatter.description}
+						key={`${post.node.frontmatter.date}__${post.node.frontmatter.title}`}
+						path={post.node.frontmatter.path}
+						tags={post.node.frontmatter.tags}
+					/>   
+					
+				)}   
+		</Layout>
 	)
 }
 
