ae82a98acbfb6055676a266f3f855995fc8f6000
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-10 17:19:23 +0300

    Replace scss with tailwind

--- src/components/PostPreview/PostPreview.tsx
+++ src/components/PostPreview/PostPreview.tsx
@@ -12,32 +12,34 @@ interface IPostPreviewProps {
 const PostPreview: React.FC<IPostPreviewProps> = props => {
 	const { image, title, description, path, tags } = props
 	return (
-		<div className='post-preview'>
+		<div className='max-w-sm rounded overflow-hidden shadow-lg mt-10'>
 			<img
 				className='post-preview__image'
 				src={image}
 				alt={title}
 			/>
 			
-			<h3 className='post-preview__title'>{title}</h3>
-			<p className='post-preview__description'>{description}</p>
-
-			<div className='post-preview__tags'>
+			<h3 className='px-6 py-4 font-bold text-purple-500 text-xl uppercase'>{title}</h3>
+			<hr className='py-2'/>
+			<div className='px-6 mx-auto'>
 				{tags.map((tag, index) => 
 					<span
-						className='post-preview__tags__tag'
+						className='inline-block bg-gray-200 rounded-full text-sm font-semibold text-gray-700 mr-2'
 						key={index}
 					>
 						{tag}
 					</span>
 				)}
 			</div>
+			<p className='px-6 py-4 text-gray-700 text-base text-justify'>{description}</p>
+
+			
 			
 			<Link
-				className='post-preview__read-more__link'
+				className='max-w-sm px-4 py-4 float-right'
 				to={path}
 			>
-				<button className='post-preview__read-more'>Read More</button>
+				<button className='bg-transparent hover:bg-purple-500 text-purple-900 font-semibold hover:text-white py-2 px-2 border border-purple-500 hover:border-transparent rounded'>Read More</button>
 			</Link>
 		</div>
 	)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,19 +28,22 @@ const Landpage: React.FC<ILandpageProps> = props => {
 	const { data } = props
 	return (
 		<Layout>
-			{ 
-				data.allMarkdownRemark.edges.map(post => 
-					<PostPreview
-						image={post.node.frontmatter.image}
-						title={post.node.frontmatter.title}
-						description={post.node.frontmatter.description}
-						key={`${post.node.frontmatter.date}__${post.node.frontmatter.title}`}
-						path={post.node.frontmatter.path}
-						tags={post.node.frontmatter.tags}
-					/>   
+			<div style={{ marginLeft:'380px'}} className='grid grid-cols-2 gap-20 float-right'>
+				{ 
+					data.allMarkdownRemark.edges.map(post => 
+						<PostPreview
+							image={post.node.frontmatter.image}
+							title={post.node.frontmatter.title}
+							description={post.node.frontmatter.description}
+							key={`${post.node.frontmatter.date}__${post.node.frontmatter.title}`}
+							path={post.node.frontmatter.path}
+							tags={post.node.frontmatter.tags}
+						/>   
 					
-				)}   
+					)}   
+			</div>
 		</Layout>
+
 	)
 }
 
