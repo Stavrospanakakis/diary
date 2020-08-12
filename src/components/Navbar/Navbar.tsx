ea13caed34d4f782fd066a6f65baa61d74c52ec8
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-12 23:48:56 +0300

    Fix duplicate code to navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,10 +6,10 @@ import NavbarData from '../../data/Navbar'
 const Navbar: React.FC = () => {
 	const {title, pages} = NavbarData
 	return (
-		<div className='bg-white w-screen shadow-lg'>
-			<div className='xl:container xl:mx-auto sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 flex items-center justify-center md:justify-between px-4 py-3 sm:p-0'>
-				<div className=''>
-					<a href='/' className='text-purple-700 font-bold uppercase'>
+		<div className='bg-white shadow-lg'>
+			<div className='xl:container xl:mx-auto sm:flex sm:items-center sm:px-4 sm:py-3 flex items-center justify-center md:justify-between px-4 py-3 sm:p-0'>
+				<div>
+					<a href='/' className='text-purple-700 font-bold uppercase text-center'>
 						{title}
 					</a>
 				</div>
