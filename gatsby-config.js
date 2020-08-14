119a286098bc750a32f713a59adf4596caf77100
Stavros Panakakis <stavrospanakakis@gmail.com>
2020-08-14 22:48:31 +0300

    Change posts path / remove fake posts/ add my first post

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
 			resolve: 'gatsby-source-filesystem',
 			options: {
 				name: 'posts',
-				path: `${__dirname}/src/posts`,
+				path: `${__dirname}/posts`,
 			},
 		},
 		{
